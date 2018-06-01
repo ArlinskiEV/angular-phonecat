f9753eab2a976bf3ce75bf28792254aa8dee709d
ArlinskiEV <ArlinskiEV@gmail.com>
2018-06-01 17:39:05 +0300

    up:phone-list-component, trouble:path (templateUrl:..), stable

--- app/phone-list/phone-list.component.ts
+++ app/phone-list/phone-list.component.ts
@@ -1,11 +1,17 @@
-declare var angular: angular.IAngularStatic;
+import { Component } from '@angular/core';
 import { Phone, PhoneData } from '../core/phone/phone.service';
-
-class PhoneListController {
+declare var angular: angular.IAngularStatic;
+import { downgradeComponent } from '@angular/upgrade/static';
+ 
+@Component({
+  selector: 'phone-list',
+  templateUrl: './phone-list/phone-list.template.html',
+})
+export class PhoneListComponent {
   phones: PhoneData[];
+  query: string;
   orderProp: string;
 
-  static $inject = ['phone'];
   constructor(phone: Phone) {
     phone.query().subscribe(phones => {
       this.phones = phones;
@@ -13,11 +19,41 @@ class PhoneListController {
     this.orderProp = 'age';
   }
 
+  public getPhones(): PhoneData[] {
+    return this.sortPhones(this.filterPhones(this.phones));
+  }
+  
+  private filterPhones(phones: PhoneData[]) {
+    if (phones && this.query) {
+      return phones.filter(phone => {
+        let name = phone.name.toLowerCase();
+        let snippet = phone.snippet.toLowerCase();
+        return name.indexOf(this.query) >= 0 || snippet.indexOf(this.query) >= 0;
+      });
+    }
+    return phones;
+  }
+  
+  private sortPhones(phones: PhoneData[]) {
+    if (phones && this.orderProp) {
+      return phones
+        .slice(0) // Make a copy
+        .sort((a, b) => {
+          if (a[this.orderProp] < b[this.orderProp]) {
+            return -1;
+          } else if ([b[this.orderProp] < a[this.orderProp]]) {
+            return 1;
+          } else {
+            return 0;
+          }
+        });
+    }
+    return phones;
+  }
 }
 
-angular.
-  module('phoneList').
-  component('phoneList', {
-    templateUrl: 'phone-list/phone-list.template.html',
-    controller: PhoneListController
-  });
+angular.module('phoneList')
+  .directive(
+    'phoneList',
+    downgradeComponent({component: PhoneListComponent}) as angular.IDirectiveFactory
+  );
